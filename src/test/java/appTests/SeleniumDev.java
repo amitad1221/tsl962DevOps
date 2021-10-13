090bd82ec546fcf9a227446c844739d9e2b70447
amit <dasamit1770@gmail.com>
2021-10-13 15:38:38 +0530

    commit

---
+++ src/test/java/appTests/SeleniumDev.java
@@ -0,0 +1,30 @@
+package appTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SeleniumDev {
+  WebDriver driver;	
+  @Test
+  public void titleTest() {
+	  driver.get("http://selenium.dev");
+	  Assert.assertEquals(driver.getTitle(), "Selenium");
+	  
+  }
+  
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver","src\\test\\resources\\drivers\\chromedriver.exe");
+	  driver=new ChromeDriver();  
+  }
+  
+  @AfterTest
+  public void afterTest() throws InterruptedException {
+	  Thread.sleep(2000);
+	  driver.quit();
+  }
+}
